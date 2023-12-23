11f2430b600c6b2c94f9320a634cc21ec8e50eb2
Illanser <kalamata1821pet@gmail.com>
2023-12-23 10:07:08 +0200

    Added timeout on chat request

--- src/progressiveai/chat.py
+++ src/progressiveai/chat.py
@@ -17,31 +17,26 @@ class Chat:
         }
         try:
             response = self.session.get(
-                f'https://api.progressiveai.org/v1/{self.model}/chat', params=params)
-            # This will raise an HTTPError if the status code is 4XX or 5XX
+                f'https://api.progressiveai.org/v1/{self.model}/chat', params=params, timeout=95)
             response.raise_for_status()
             data = response.json()
         except requests.exceptions.HTTPError as errh:
             if response.status_code == 404:
                 print("The AI Model is unavailable or doesn't exist at all!")
-            elif response.status_code == 524:  # Cloudflare Timeout Error
-                print("The request timed out. Please try again later.")
-            elif response.status_code == 502:
-                print("Oops! The Server failed to return a response!")
             else:
-                print("HTTP Error: ", errh)
+                print(f"HTTP Error: {errh}")
             return None
-        except json.JSONDecodeError as e:
-            print(f"JSON decoding error: {e}")
-            print(f"Status code: {response.status_code}")
-            print(f"Response content: {response.content}")
+        except requests.exceptions.ConnectionError as errc:
+            print(f"Error Connecting: {errc}")
             return None
-        except requests.RequestException as e:
-            print(f"Request error: {e}")
+        except requests.exceptions.Timeout as errt:
+            print(f"Timeout Error: {errt}")
+            return None
+        except requests.exceptions.RequestException as err:
+            print(f"Something went wrong with the request: {err}")
             return None
         except Exception as e:
-            print(f"Status code: {response.status_code}")
-            print(f"Response content: {response.content}")
+            print(f"An error occurred: {e}")
             return None
 
         if 'error' in data:
