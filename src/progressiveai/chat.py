ab7258a5f57315c602fcd017f37cd6e38a4c7fd8
Illanser <kalamata1821pet@gmail.com>
2023-12-23 10:00:01 +0200

    Added error handling

--- src/progressiveai/chat.py
+++ src/progressiveai/chat.py
@@ -13,24 +13,37 @@ class Chat:
     def get_response(self):
         params = {
             'api_key': self.api_key,
-            'prompt': self.text,
+            'prompt': self.text
         }
-        response = self.session.get(
-            f'https://api.progressiveai.org/v1/{self.model}/chat', params=params)
         try:
+            response = self.session.get(
+                f'https://api.progressiveai.org/v1/{self.model}/chat', params=params)
+            # This will raise an HTTPError if the status code is 4XX or 5XX
+            response.raise_for_status()
             data = response.json()
+        except requests.exceptions.HTTPError as errh:
+            if response.status_code == 404:
+                print("The AI Model is unavailable or doesn't exist at all!")
+            elif response.status_code == 524:  # Cloudflare Timeout Error
+                print("The request timed out. Please try again later.")
+            elif response.status_code == 502:
+                print("Oops! The Server failed to return a response!")
+            else:
+                print("HTTP Error: ", errh)
+            return None
         except json.JSONDecodeError as e:
             print(f"JSON decoding error: {e}")
             print(f"Status code: {response.status_code}")
             print(f"Response content: {response.content}")
-            raise
+            return None
         except requests.RequestException as e:
             print(f"Request error: {e}")
-            raise
+            return None
         except Exception as e:
             print(f"Status code: {response.status_code}")
             print(f"Response content: {response.content}")
-            raise
+            return None
+
         if 'error' in data:
             raise APIError(data['error'])
         return data['response']
--- src/progressiveai/complete.py
+++ src/progressiveai/complete.py
@@ -19,22 +19,31 @@ class Complete:
             f'https://api.progressiveai.org/v1/{self.model}/complete', params=params)
         try:
             data = response.json()
+        except requests.exceptions.HTTPError as errh:
+            if response.status_code == 404:
+                print("The AI Model is unavailable or doesn't exist at all!")
+                raise
+            elif response.status_code == 524:  # Cloudflare Timeout Error
+                print("The request timed out. Please try again later.")
+                raise
+            elif response.status_code == 502:
+                print("Opps! The Server failed to return a response!")
+            else:
+                print("HTTP Error: ", errh)
+                raise
         except json.JSONDecodeError as e:
             print(f"JSON decoding error: {e}")
             print(f"Status code: {response.status_code}")
             print(f"Response content: {response.content}")
             raise
-        except requests.exceptions.HTTPError as errh:
-            print("HTTP Error:", errh)
-            raise
         except requests.exceptions.ConnectionError as errc:
-            print("Error Connecting:", errc)
+            print("Error Connecting: ", errc)
             raise
         except requests.exceptions.Timeout as errt:
-            print("Timeout Error:", errt)
+            print("Timeout Error: ", errt)
             raise
         except requests.exceptions.RequestException as err:
-            print("Something went wrong with the request:", err)
+            print("Something went wrong with the request: ", err)
             raise
         if 'error' in data:
             raise APIError(data['error'])
